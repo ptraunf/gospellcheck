0cbbbb43c7399355bddee4db9735fc9586f05163
_ <pete@p-sys76.localdomain>
2024-03-26 15:07:12 -0400

    Enumerate trie

--- trie_test.go
+++ trie_test.go
@@ -3,15 +3,35 @@ package gospellcheck
 import "testing"
 
 func TestCreate(t *testing.T) {
-	var sc *Trie = Trie.New()
+	var sc *Trie = newTrie()
 	if sc == nil {
 		t.Fatalf("StringContainer.New ")
 	}
 }
 func TestInsert(t *testing.T) {
-
+	// strings := []string{"ant", "anthem", "anteater"}
+	testString := "and"
+	var trie *Trie = newTrie()
+	result := trie.Insert(testString)
+	if !result {
+		t.Fatalf("\nExpected:\ttrue\nActual:\t%v", result)
+	}
 }
-
+func TestEnumerate(t *testing.T) {
+	strings := []string{"ant", "anthem", "anteater"}
+	var trie *Trie = newTrie()
+	var insertResult bool = true
+	for _, word := range strings {
+		insertResult = insertResult && trie.Insert(word)
+	}
+	results := trie.Enumerate()
+	expectedNResults := len(strings)
+	actualNResults := len(results)
+	if expectedNResults != actualNResults {
+		t.Fatalf("\nExpected %v results; got %v results", expectedNResults, actualNResults)
+	}
+	t.Logf("Trie:\n%v\n", trie)
+}
 func TestRemove(t *testing.T) {
 
 }
